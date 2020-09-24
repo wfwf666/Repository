a3d2084c5533a2718fc9ed2ba769c9f2d34ba832
unknown <unknown@example.com>
2020-09-24 16:30:39 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,29 @@
+Page({
+  data: {
+    array: [{
+      name:'岗前职业技能培训',
+      prize:'￥50',
+      img: '../img/小老虎.jpg',
+    }, {
+      name:'岗前职业技能培训',
+      prize:'￥50',
+      img: '../img/小老虎.jpg',
+    }, {
+      name:'岗前职业技能培训',
+      prize:'￥50',
+      img: '../img/小老虎.jpg',
+    }, {
+      name:'岗前职业技能培训',
+      prize:'￥50',
+      img: '../img/小老虎.jpg',
+    }, {
+      name:'岗前职业技能培训',
+      prize:'￥50',
+      img: '../img/小老虎.jpg',
+    }, {
+      name:'岗前职业技能培训',
+      prize:'￥50',
+      img: '../img/小老虎.jpg',
+    }]
+  }
+})
